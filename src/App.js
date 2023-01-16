cdb0363cb576f65b164d9d42136d68f3a2fee682
adamk <adamknapik244@gmail.com>
2023-01-16 08:48:41 +0100

    Fix useEffect, Add async/await

--- src/App.js
+++ src/App.js
@@ -1,132 +1,125 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import PokemonTypesIcons from './data/PokemonTypesIcons';
-import Picker from './components/Picker/Picker';
-import Card from './components/Card/Card';
-import Pagination from './components/Pagination/Pagination';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PokemonTypesIcons from "./data/PokemonTypesIcons";
+import Picker from "./components/Picker/Picker";
+import Card from "./components/Card/Card";
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
   const baseURL = "https://pokeapi.co/api/v2";
-    const pokemonsLimit = 904; //904
-  
-    const [pokemonList, setPokemonList] = useState([{}]);
-    const [pokemon, setPokemon] = useState('1');
-    const [pokemonData, setPokemonData] = useState({});
-    const [pokemonType, setPokemonType] = useState('');
-    const [pokemonDescritpion, setPokemonDescritpion] = useState('');
-    
-    useEffect(() => {
-        getPokemonList();
+  const pokemonsLimit = 904; //904
+
+  const [pokemonList, setPokemonList] = useState([{}]);
+  const [pokemon, setPokemon] = useState("1");
+  const [pokemonData, setPokemonData] = useState({});
+  const [pokemonType, setPokemonType] = useState("");
+  const [pokemonDescritpion, setPokemonDescritpion] = useState("");
+  const [pokemonImage, setPokemonImage] = useState("");
+
+  useEffect(() => {
+    let unsubscribe = false;
+    const getPokemonList = async () => {
+      const { data } = await axios.get(
+        `${baseURL}/pokemon/?offset=0&limit=${pokemonsLimit}`
+      );
+      if (!unsubscribe) {
+        setPokemonList(data.results);
         getPokemon(pokemon);
-    }, [pokemon]);
-
-    const getPokemonList = () => {
-        axios.get(`${baseURL}/pokemon/?offset=0&limit=${pokemonsLimit}`).then((res) => {
-            setPokemonList(res.data.results);
-        }); 
+      }
     };
+    getPokemonList();
 
-    const getPokemon = (pokemonId) => {
-        axios.get(`${baseURL}/pokemon/${pokemonId}`).then((res) => {
-            setPokemonData({ 
-                id: res.data.id,
-                name: res.data.name,
-                height: res.data.height,
-                weight: res.data.weight,
-                hp: res.data.stats[0].base_stat,
-                attack: res.data.stats[1].base_stat,
-                defense: res.data.stats[2].base_stat,
-                specialAttack: res.data.stats[3].base_stat,
-                specialDefense: res.data.stats[4].base_stat,
-                speed: res.data.stats[5].base_stat
-            });
-
-            setPokemonType( 
-                res.data.types.map((type, index) => {
-                    return (
-                        <li key={index}>
-                            {PokemonTypesIcons[type.type.name]} {type.type.name}  
-                        </li>
-                    )
-                })
-            );
-
-            getPokemonDescritpion(res.data.name);
-        })
+    return () => {
+      unsubscribe = true;
     };
+  }, [pokemon]);
 
-    const getPokemonDescritpion = (pokemonName) => {
-        axios.get(`${baseURL}/pokemon-species/${pokemonName}`).then((res) => {
-            setPokemonDescritpion(res.data.flavor_text_entries[0].flavor_text.replace(/[\n\f]/g, ' '));    
-        })
-    };
+  const getPokemon = async (newPokemon) => {
+    const res = await axios.get(`${baseURL}/pokemon/${newPokemon}`);
+    const res2 = await axios.get(`${baseURL}/pokemon-species/${newPokemon}`);
 
-    function getPokemonImageUrl(pokemonId) {
-        return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-    };
+    setPokemonData({
+      id: res.data.id,
+      name: res.data.name,
+      height: res.data.height,
+      weight: res.data.weight,
+      hp: res.data.stats[0].base_stat,
+      attack: res.data.stats[1].base_stat,
+      defense: res.data.stats[2].base_stat,
+      specialAttack: res.data.stats[3].base_stat,
+      specialDefense: res.data.stats[4].base_stat,
+      speed: res.data.stats[5].base_stat,
+    });
+    setPokemonType(
+      res.data.types.map((type, index) => {
+        return (
+          <li key={index}>
+            {PokemonTypesIcons[type.type.name]} {type.type.name}
+          </li>
+        );
+      })
+    );
+    setPokemonDescritpion(res2.data.flavor_text_entries[0].flavor_text.replace(/[\n\f]/g, ' '));
+    setPokemonImage(
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${newPokemon}.png`
+    );
+  };
 
+  const handleChange = (e) => {
+    setChange(e.target.value);
+  };
 
-    const handleChange = (e) => {
-        setChange(e.target.value);
+  const prevPokemon = () => {
+    let prevPokemon = pokemonData.id - 1;
+    if (prevPokemon > 0) {
+      setChange(prevPokemon);
     }
-    
-    const prevPokemon = () => {
-        let prevPokemon = pokemonData.id - 1;
-        if(prevPokemon > 0) { 
-            setChange(prevPokemon);
-        }
-    };
+  };
 
-    const nextPokemon = () => {
-        let nextPokemon = parseInt(pokemonData.id) + 1;
-        if(nextPokemon <= pokemonsLimit) {
-            setChange(nextPokemon);
-        }
-    };
+  const nextPokemon = () => {
+    let nextPokemon = parseInt(pokemonData.id) + 1;
+    if (nextPokemon <= pokemonsLimit) {
+      setChange(nextPokemon);
+    }
+  };
 
-    const setChange = (newPokemon) => {
-        setPokemon(newPokemon);
-        getPokemon(newPokemon);
-    };
+  const setChange = (newPokemon) => {
+    setPokemon(newPokemon);
+    getPokemon(newPokemon);
+  };
 
-    return (
-    <div className='container'>
-        <Picker 
-            onChange = {handleChange}
-            value = {pokemon}
-            option = {
-                pokemonList.map((poke, index) => {
-                    return (
-                        <option key={index} value={index+1}>
-                            #{index+1} - {poke.name}
-                        </option> 
-                    )
-                }) 
-            }
-        />
+  return (
+    <div className="container">
+      <Picker
+        onChange={handleChange}
+        value={pokemon}
+        option={pokemonList.map((poke, index) => {
+          return (
+            <option key={index} value={index + 1}>
+              #{index + 1} - {poke.name}
+            </option>
+          );
+        })}
+      />
 
-       <Pagination 
-            btnPrev= {prevPokemon}
-            btnNext= {nextPokemon}
-        />
-
-        <Card 
-            img = {getPokemonImageUrl(pokemon)}
-            title = {pokemonData.name} 
-            id = {pokemonData.id}
-            description = {pokemonDescritpion}
-            type = {pokemonType}
-            height = {pokemonData.height}
-            weight = {pokemonData.weight}
-            hp = {pokemonData.hp}
-            attack = {pokemonData.attack}
-            defense = {pokemonData.defense}
-            specialAttack = {pokemonData.specialAttack}
-            specialDefense = {pokemonData.specialDefense}
-            speed = {pokemonData.speed}
-        /> 
+      <Pagination btnPrev={prevPokemon} btnNext={nextPokemon} />
+      <Card
+        img={pokemonImage}
+        title={pokemonData.name}
+        id={pokemonData.id}
+        description={pokemonDescritpion}
+        type={pokemonType}
+        height={pokemonData.height}
+        weight={pokemonData.weight}
+        hp={pokemonData.hp}
+        attack={pokemonData.attack}
+        defense={pokemonData.defense}
+        specialAttack={pokemonData.specialAttack}
+        specialDefense={pokemonData.specialDefense}
+        speed={pokemonData.speed}
+      />
     </div>
-    )
+  );
 }
 
 export default App;
